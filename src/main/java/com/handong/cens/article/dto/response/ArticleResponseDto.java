cc51223773b4f56d8da1564bbd6ca4bd5a2f2a7b
Sumin Jeon <wjstnals1211@gmail.com>
2025-05-04 16:05:38 +0900

    feat: ArticleResponseDto 추가

---
+++ src/main/java/com/handong/cens/article/dto/response/ArticleResponseDto.java
@@ -0,0 +1,18 @@
+package com.handong.cens.article.dto.response;
+
+import com.handong.cens.article.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ArticleResponseDto {
+
+    private Long articleId;
+    private String title;
+    private String content;
+    private String date;
+    private String category;
+}
