e13349e5b8c53ae05c7533462616e546cba88f0c
Sumin Jeon <wjstnals1211@gmail.com>
2025-05-04 16:05:57 +0900

    feat: add methods to retrieve articles by category and convert to DTO

--- src/main/java/com/handong/cens/article/controller/ArticleController.java
+++ src/main/java/com/handong/cens/article/controller/ArticleController.java
@@ -1,24 +1,40 @@
 package com.handong.cens.article.controller;
 
+import com.handong.cens.article.dto.response.ArticleResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.handong.cens.article.service.ArticleService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/article")
 public class ArticleController {
 
     private final ArticleService articleService;
 
-    @GetMapping("/api/articles")
+    @GetMapping("/save")
     public void saveArticles() {
         try {
             articleService.saveArticles();
         } catch (Exception e) {
             log.error(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ArticleResponseDto>> getArticles(@RequestParam(value = "code", required = false) Integer code) {
+        if (code == null) {
+            return ResponseEntity.ok().body(articleService.getAllArticles());
+        } else {
+            return ResponseEntity.ok().body(articleService.getArticlesByCategoryCode(code));
         }
     }
 }
