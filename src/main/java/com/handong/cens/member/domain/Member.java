9ddbe00bc49f3ddb68598c1118611cd2756a04ba
Sumin Jeon <wjstnals1211@gmail.com>
2025-05-27 13:14:19 +0900

    feat: 이메일 및 providerId 필드에 고유한 제약 조건을 추가

--- src/main/java/com/handong/cens/member/domain/Member.java
+++ src/main/java/com/handong/cens/member/domain/Member.java
@@ -18,6 +18,7 @@ public class Member {
     @Column(name = "login_id")
     private String loginId;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
@@ -31,5 +32,14 @@ public class Member {
     private String provider;
 
     // providerId : 구굴 로그인 한 유저의 고유 ID가 들어감
+    @Column(unique = true)
     private String providerId;
+
+    // 엔티티 저장 전 기본 권한 설정
+    @PrePersist
+    public void setDefaultRole() {
+        if (this.role == null) {
+            this.role = MemberRole.USER;
+        }
+    }
 }
